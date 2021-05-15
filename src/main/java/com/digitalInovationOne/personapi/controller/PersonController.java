edddbe3adbfaab925b9c085fccbe012484c3f565
Jefferson <jeffersonsilva.2015@outlook.com>
2021-05-14 23:19:03 -0300

    push

--- src/main/java/com/digitalInovationOne/personapi/controller/PersonController.java
+++ src/main/java/com/digitalInovationOne/personapi/controller/PersonController.java
@@ -1,6 +1,6 @@
 package com.digitalInovationOne.personapi.controller;
 
-
+import com.digitalInovationOne.personapi.entity.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +11,8 @@ public class PersonController {
 
     @GetMapping
     public String getBook(){
-    return "API Test!";
+        Person person = new Person();
+        person.getId();
+        return "API Test!";
     }
 }
