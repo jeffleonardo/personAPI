9c69d2938e53f6f41cc3011568552a09106306b5
Jefferson <jeffersonsilva.2015@outlook.com>
2021-05-20 01:03:54 -0300

    update

--- src/main/java/com/digitalInovationOne/personapi/service/PersonService.java
+++ src/main/java/com/digitalInovationOne/personapi/service/PersonService.java
@@ -43,10 +43,9 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Optional<Person> optionalPerson = personRepository.findById(id);
-        if(optionalPerson.isEmpty()){
-            throw new PersonNotFoundException(id);
-        }
-        return personMapper.toDTO(optionalPerson.get());
+        Person person = personRepository.findById(id)
+                .orElseThrow(()->new PersonNotFoundException(id));
+
+        return personMapper.toDTO(person);
     }
 }
